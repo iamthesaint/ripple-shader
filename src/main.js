c4adf82ac99353189da4a017c270d2230535b5bb
iamthesaint <stephenie2@me.com>
2025-04-30 11:10:30 -0400

    shader outlines

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './style.css'
+import './script.js'
+
+document.querySelector('#app').innerHTML = `
+      <nav>
+      <div class="logo">
+        <p>s/s</p>
+      </div>
+      <div class="nav-items">
+        <p>about</p>
+        <p>works</p>
+        <p>contact</p>
+      </div>
+    </nav>
+    `
